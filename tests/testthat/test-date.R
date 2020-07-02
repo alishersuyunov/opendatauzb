fbacd0420317e176ea18876e757b61abee419e6f
Alisher Suyunov <alisher.c@ya.ru>
2020-07-02 19:50:12 +0500

    Unit test functionality check

---
+++ tests/testthat/test-date.R
@@ -0,0 +1,28 @@
+library("opendatauzb")
+context("Date and Class checks")
+
+# test_that("Date is processed properly", {
+#   expect_error(getTicker("UZ7011340005",
+#                          from = "2020-01-01",
+#                          to = "30.06.2020"))
+#   expect_error(getMarketIndex("finance",
+#                               from = "2020-01-01",
+#                               to = "30.06.2020"))
+#
+# })
+#
+# test_that("Returned objects are correct", {
+#   expect_s3_class(getMarketIndex("finance",
+#                                     from = "01.06.2020",
+#                                     to = "30.06.2020"), "data.frame")
+#   expect_s3_class(getTicker("UZ7011340005",
+#                                from = "01.06.2020",
+#                                to = "30.06.2020"), "data.frame")
+#
+#   expect_s3_class(currentBidsAsks(), "data.frame")
+#   expect_s3_class(ipo(), "data.frame")
+# })
+
+test_that("Date", {
+  expect_equal(TRUE, TRUE)
+})
